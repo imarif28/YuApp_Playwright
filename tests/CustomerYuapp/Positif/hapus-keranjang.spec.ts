00f8528c1af744451749e0ad866fa8d1a5860729
Ilham Muhammad Arif <osis.ilham99@gmail.com>
2025-11-17 09:50:37 +0700

    Merapikan

--- tests/CustomerYuapp/Positif/hapus-keranjang.spec.ts
+++ tests/CustomerYuapp/Positif/hapus-keranjang.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 import { CustomerPage } from '../../../pages/CustomerPage';
 
     // Nama produk yang dibeli (bisa penggalan kata, case-insensitive) Tas Persegi Kecil Gaya Korea, Lintas Batas Tas
-    const nama_barang = process.env.BARANG || 'Tas Persegi Kecil Gaya Korea';
+    const nama_barang = process.env.BARANG || 'Lintas Batas Tas';
 
     test('Menghapus item dikeranjang', async ({ page }) => {
         const customerPage = new CustomerPage(page);
